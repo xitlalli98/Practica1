302c2c4469234c813cbb36731fc8b4b2abdf1a69
xitlalli98 <reginar98@hotmail.com>
2016-09-07 18:29:12 -0700

    Practica visual

---
+++ practicavisual/practicauno/practicauno/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicauno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sueldo;
+            string linea;
+            Console.Write("Ingrese el sueldo:");
+            linea = Console.ReadLine();
+            sueldo = float.Parse(linea);
+            if (sueldo > 3000)
+            {
+                Console.Write("Esta persona debe abonar impuestos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
